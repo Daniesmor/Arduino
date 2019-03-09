ceaf970364c63ac2b2f705efa8c186e6334f08c8
TheDaniGamer <daniesmor@gmail.com>
2019-03-09 19:33:27 +0100

    Create EsquivaObstaculos.ino
    El robot esquiva los obstáculos

---
+++ EsquivaObstaculos.ino
@@ -0,0 +1,41 @@
+#include <Servo.h>
+#include <NewPing.h>
+
+#define TRIGGER_PIN  6
+#define ECHO_PIN     5
+#define MAX_DISTANCE 500
+
+Servo servoD;
+Servo servoI;
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+int uS, distancia;
+void setup() {
+  //Serial.begin(9600);
+  servoD.attach(8);
+  servoI.attach(7);
+  servoD.write(0);
+  servoI.write(180);
+ 
+
+ 
+}
+
+
+void loop() {
+uS = sonar.ping_median();
+distancia = (uS/ US_ROUNDTRIP_CM);
+if (distancia <  10) {
+    servoD.write(180);
+    servoI.write(0);
+} 
+else if (distancia >= 10 && distancia <=12) {
+    servoD.write(90);
+    servoI.write(90);
+}
+else if (distancia > 12) {
+    servoD.write(0);
+    servoI.write(180);
+}
+delay (50);
+}
